ad3b9e333d798a8b979ae09398fe0221ee8cb5cf
MitanshuShaBa <42906426+MitanshuShaBa@users.noreply.github.com>
2022-09-05 19:56:05 -0400

    added Sym Class implementation

---
+++ src/Sym.py
@@ -0,0 +1,35 @@
+import math
+
+
+class Sym:
+    def __init__(self, c=0, s=""):
+        self.n = 0
+        self.at = c
+        self.name = s
+        self._has = {}
+
+    def add(self, v):
+        if v is not None:
+            self.n += 1
+            if v in self._has:
+                self._has[v] += 1
+            else:
+                self._has[v] = 1
+
+    def mid(self):
+        most = -1
+        for key, value in self._has.items():
+            if value > most:
+                mode = key
+                most = value
+        return mode
+
+    def div(self):
+        def p_log_p(p):
+            return p * math.log(p, 2)
+
+        e = 0
+        for _, n in self._has.items():
+            if n > 0:
+                e = e-p_log_p(n/self.n)
+        return e
