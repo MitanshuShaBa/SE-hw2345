a813e32cbd6ba343983697ba65e120f7ef3fdb40
Janvi08 <janviphadtare9@gmail.com>
2022-09-12 21:16:30 -0400

    Added stats function in Data Class

---
+++ src/Data.py
@@ -0,0 +1,16 @@
+import math
+
+
+class Data:
+    def stats(self, places, showCols, fun):
+        showCols, fun= showCols or self.cols.y, fun or "mid"
+        t={}
+        for _, col in showCols.items():
+            v=fun(col)
+            v=rnd(v, places) or v
+            t[col.name]=v
+        return t
+
+def rnd(x, places):
+    mul=10**(places or 2)
+    return math.floor(x*mul+0.5)/mul
