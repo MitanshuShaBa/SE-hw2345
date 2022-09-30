aeedc085d3a4504b113ea61878af73af4af0c61c
MitanshuShaBa <42906426+MitanshuShaBa@users.noreply.github.com>
2022-09-29 22:16:28 -0400

    test the testcases with wrong code

--- src/test.py
+++ src/test.py
@@ -2,28 +2,32 @@ import num
 import Sym
 import main
 
+
 def big_num():
     obj = num.num()
     main.the["nums"] = 32
     for i in range(1, 1001):
         obj.add(i)
-    assert len(obj._has) == 32
+    assert len(obj._has) == 31
+
 
 def test_Sym():
     obj = Sym.Sym()
-    for value in ["a","a","a","a","b","b","c"] :
+    for value in ["a", "a", "a", "a", "b", "b", "c"]:
         obj.add(value)
     mode, entropy = obj.mid(), obj.div()
     entropy = (entropy*1000)//1/1000
-    assert( mode=="a" and 1.37 <= entropy and entropy <=1.38)   
+    assert(mode == "a" and 1.37 <= entropy and entropy <= 1.38)
+
 
 def test_num():
     obj = num.num()
     main.the["nums"] = 512
     for i in range(1, 101):
         obj.add(i)
-    mid, div=obj.mid(), obj.div()
-    assert mid <= 52 and mid >=50 and div<32 and div > 30.5
+    mid, div = obj.mid(), obj.div()
+    assert mid <= 52 and mid >= 50 and div < 32 and div > 30.5
+
 
 if __name__ == "__main__":
     big_num()
