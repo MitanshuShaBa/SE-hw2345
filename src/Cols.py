73d1f7cb925dde2869a78fc5b61a337b9e37b0f9
MitanshuShaBa <42906426+MitanshuShaBa@users.noreply.github.com>
2022-09-29 22:38:39 -0400

    format file based on convention

--- src/Cols.py
+++ src/Cols.py
@@ -2,26 +2,24 @@ import re
 import num
 import Sym
 
+
 class Cols:
     def __init__(self, names):
         self.names = names
         self.all = {}
-        self.klass =  None
+        self.klass = None
         self.x = {}
         self.y = {}
         for k, value in enumerate(self.names):
             if re.search("^[A-Z]*", value):
                 self.all[k] = value
-                num(k,value)
+                num(k, value)
             else:
-                Sym(k,value)
+                Sym(k, value)
             if not re.search(":$", value):
                 if re.search("[!+-]", value):
-                    self.y[k] = value 
+                    self.y[k] = value
                 else:
-                    self.x[k] = value 
+                    self.x[k] = value
                 if re.search("!$", value):
-                    self.klass[k]=value
-
-
-
+                    self.klass[k] = value
--- src/Data.py
+++ src/Data.py
@@ -5,41 +5,35 @@ import Cols
 
 class Data:
     def __init__(self, src):
- 
-       self.cols = 0
-       self.rows={}
-       if(type(src) == str):
-           csv(src, self.add(row))
-       else:
-           for _,row in src.items():
-               self.add(row)
-          
+
+        self.cols = 0
+        self.rows = {}
+        if(type(src) == str):
+            csv(src, self.add(row))
+        else:
+            for _, row in src.items():
+                self.add(row)
 
     def stats(self, places, showCols, fun):
-        showCols, fun= showCols or self.cols["y"], fun or "mid"
-        t={}
+        showCols, fun = showCols or self.cols["y"], fun or "mid"
+        t = {}
         for _, col in showCols.items():
-            v=fun(col)
-            v=rnd(v, places) or v
-            t[col["name"]]=v
+            v = fun(col)
+            v = rnd(v, places) or v
+            t[col["name"]] = v
         return t
 
     def add(self, xs, row):
- 
-       if self.cols is None :
-           self.cols = Cols(xs)
-       else:
-           row= self.rows.push(xs.cells, xs)
-           for _, todo in zip(self.cols.x , self.cols.y):
-               for _,col in todo.items():
-                   col.add(row.cells[col.at])
-    
+
+        if self.cols is None:
+            self.cols = Cols(xs)
+        else:
+            row = self.rows.push(xs.cells, xs)
+            for _, todo in zip(self.cols.x, self.cols.y):
+                for _, col in todo.items():
+                    col.add(row.cells[col.at])
+
 
 def rnd(x, places):
-    mul=10**(places or 2)
+    mul = 10**(places or 2)
     return math.floor(x*mul+0.5)/mul
-
-
-
-
-
